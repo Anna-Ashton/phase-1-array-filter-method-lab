3847f5b039ab3857f13c6ad27aa6d3d19a0fa9d2
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-19 10:15:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+let findMatching = (nameOfDrivers, gotDriver) => {
+    return nameOfDrivers.filter(driverName => {return driverName.toLowerCase()===gotDriver.toLowerCase()});
+}
+
+let fuzzyMatch = (nameOfDrivers, lettersMatching) => {
+return nameOfDrivers.filter(driverName=>{return driverName.substring(0,2) ===lettersMatching});
+}
+
+let matchName = (nameOfDrivers, nameMatches) => {
+    return nameOfDrivers.filter(driverName=>{return driverName.name=== nameMatches});
+}
